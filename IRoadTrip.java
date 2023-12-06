cffb67ed2500310a5ceb18fef97e4980725243e7
Rausell <arlopez6@dons.usfca.edu>
2023-12-06 13:58:02 -0800

    borders.txt readability
    Reading borders.txt & printing back the country neighbor information for reliable usage of hashMap.

--- IRoadTrip.java
+++ IRoadTrip.java
@@ -8,23 +8,60 @@ import java.util.HashMap;
 //Import arrays module
 import java.util.ArrayList;
 import java.util.Arrays;
+//Importing map module for graph creation
+import java.util.Map;
 
 public class IRoadTrip {
-    //Creating hashmap to store the country code and country name
+    //Creating hashmap to store the country code and country name & check existence
     private static HashMap<String, String> countries = new HashMap<String, String>();
 
-    public IRoadTrip (String [] args) {
-        // Replace with your code//DEL
-        try{
+    //Graph creation
+    private Map<String, Map<String, Integer>> adj;//Newly added
 
-            //Dealing with file state_name.tsv
-            //Opening & reading file
-            File file = new File(args[2]);
-            Scanner line = new Scanner(file);
+    public IRoadTrip (String [] args) {
+        if(args.length != 3){
+            System.err.println("Three files needed.");
+            System.exit(1);
+        }
+
+        //Initializing adj map
+        adj = new HashMap<>();
+
+        try{
+            //TESTING BELOW
+            //Reading information from file borders.txt
+            File fileI = new File(args[0]);
+            Scanner lineI = new Scanner(fileI);
+
+            while(lineI.hasNextLine()){
+                String borderInfo = lineI.nextLine();
+                String[] goodneighbors = borderInfo.toLowerCase().split(" = ");
+                String country = goodneighbors[0];
+                if(!(adj.containsKey(country))){
+                    adj.put(country, new HashMap<>());
+                }
+                if(goodneighbors.length > 1){
+                    String[] myNeighbors = goodneighbors[1].split(";");
+                    for(String neighbor : myNeighbors){
+                        System.out.println(neighbor);//DEL
+                        String[] neighborNames = neighbor.split(" ");
+                        String neighborID = neighborNames[0];
+                        //Setting distance at 0, aka starting on the user given country
+                        adj.get(country).put(neighborID, 0);
+                    }
+                    System.out.println();//DEL
+                }
+            }
+            //TESTING ABOVE
+
+
+            //Reading information from file state_name.tsv
+            File fileIII = new File(args[2]);
+            Scanner lineIII = new Scanner(fileIII);
             
             //Adding to hashmap
-            while(line.hasNextLine()){
-                String countryInfo = line.nextLine();
+            while(lineIII.hasNextLine()){
+                String countryInfo = lineIII.nextLine();
 
                 //Breaking down the line to determine if country is recent (2020)
                 String[] parts = countryInfo.toLowerCase().split("\\s+");
@@ -42,7 +79,8 @@ public class IRoadTrip {
                 }
             }
 
-            line.close();
+            lineIII.close();
+            lineI.close();//Newly Added
 
         } catch (FileNotFoundException e){
             System.err.println("Error " + e.getMessage());

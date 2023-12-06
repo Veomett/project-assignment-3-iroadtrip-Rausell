063c5de63a4b64a2a0a1701767763c85ea0cb1d9
Rausell <arlopez6@dons.usfca.edu>
2023-12-05 19:43:23 -0800

    Reading file & error handling

--- IRoadTrip.java
+++ IRoadTrip.java
@@ -1,10 +1,25 @@
 import java.util.List;
+//Importing scanner modules
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class IRoadTrip {
 
 
     public IRoadTrip (String [] args) {
-        // Replace with your code
+        // Replace with your code//DEL
+        try{
+            File file = new File(args);
+        } catch (FileNotFoundException e){
+            System.err.println(e.getMessage());
+        } catch (EmptyFileException e){
+            System.err.println(e.getMessage());
+        }
+        
+
+
+
     }
 
 
@@ -27,9 +42,12 @@ public class IRoadTrip {
 
 
     public static void main(String[] args) {
-        IRoadTrip a3 = new IRoadTrip(args);
+        IRoadTrip a3 = new IRoadTrip(args);//DEL
 
         System.out.println("Main method works!");//DEL
+
+        System.out.println("Enter the name of the first country (type EXIT to quit): ")
+        System.out.println("Enter the name of the second country (type EXIT to quit): ");
         
         a3.acceptUserInput();
     }
